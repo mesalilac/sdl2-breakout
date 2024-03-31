bb82a4376d99eab376746a28cb831805c305dfe3
abdalrzag eisa <bmjfdrh@gmail.com>
2024-03-31 07:08:08 +0300

    paddle and ball

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <SDL2/SDL.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <time.h>
 
 #define WIDTH 800
 #define HEIGHT 600
+
+#define BALL_SIZE 30
+
+#define PADDLE_WIDTH 200
+#define PADDLE_HEIGHT BALL_SIZE
 
 typedef enum
 {
@@ -14,24 +20,63 @@ typedef enum
 
 typedef struct
 {
-    SDL_Rect rect;
-} Player;
+    bool right;
+    bool left;
+} Keys;
 
-Player player_new()
+typedef struct
 {
-    Player player = {.rect = {.x = 100, .y = 100, .w = 30, .h = 30}};
+    SDL_Rect rect;
+    int xVelocity;
+    int yVelocity;
+} Ball;
 
-    return player;
+Ball ball_new()
+{
+    Ball ball = {.rect =
+                     {
+                         .x = WIDTH / 2 - PADDLE_WIDTH / 2,
+                         .y = HEIGHT - PADDLE_HEIGHT - 100,
+                         .w = BALL_SIZE,
+                         .h = BALL_SIZE,
+                     },
+                 .xVelocity = 1,
+                 .yVelocity = -1};
+
+    return ball;
 }
 
-void player_render(SDL_Renderer *ren, Player *player)
+void ball_render(SDL_Renderer *ren, Ball *ball)
+{
+    SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
+    SDL_RenderFillRect(ren, &ball->rect);
+}
+
+typedef struct
+{
+    SDL_Rect rect;
+} Paddle;
+
+Paddle paddle_new()
+{
+    Paddle paddle = {.rect = {.x = WIDTH / 2 - PADDLE_WIDTH / 2,
+                              .y = HEIGHT - PADDLE_HEIGHT - 50,
+                              .w = PADDLE_WIDTH,
+                              .h = PADDLE_HEIGHT}};
+
+    return paddle;
+}
+
+void paddle_render(SDL_Renderer *ren, Paddle *paddle)
 {
     SDL_SetRenderDrawColor(ren, 255, 0, 0, 255);
-    SDL_RenderFillRect(ren, &player->rect);
+    SDL_RenderFillRect(ren, &paddle->rect);
 }
 
 int main()
 {
+    srand(time(0));
+
     SDL_Window *win = SDL_CreateWindow("Breakout", 0, 0, WIDTH, HEIGHT, 0);
     if (win == NULL)
     {
@@ -48,7 +93,12 @@ int main()
     SDL_Event event;
     bool isRunning = true;
     GameState gameState = PLAYING;
-    Player player = player_new();
+    int score = 0;
+    Keys keys = {false};
+    int paddleSpeed = 5;
+    int ballSpeed = 4;
+    Paddle paddle = paddle_new();
+    Ball ball = ball_new();
 
     while (isRunning)
     {
@@ -59,6 +109,18 @@ int main()
             case SDL_QUIT:
                 isRunning = false;
                 break;
+            case SDL_KEYDOWN:
+                if (event.key.keysym.sym == 'a')
+                    keys.left = true;
+                if (event.key.keysym.sym == 'd')
+                    keys.right = true;
+                break;
+            case SDL_KEYUP:
+                if (event.key.keysym.sym == 'a')
+                    keys.left = false;
+                if (event.key.keysym.sym == 'd')
+                    keys.right = false;
+                break;
             }
         }
 
@@ -68,7 +130,25 @@ int main()
         switch (gameState)
         {
         case PLAYING:
-            player_render(ren, &player);
+            if (keys.left)
+                paddle.rect.x -= paddleSpeed;
+            if (keys.right)
+                paddle.rect.x += paddleSpeed;
+
+            if (ball.rect.x < 0)
+                ball.xVelocity = 1;
+            if (ball.rect.x + ball.rect.w > WIDTH)
+                ball.xVelocity = -1;
+            if (ball.rect.y < 0)
+                ball.yVelocity = 1;
+            if (ball.rect.y + ball.rect.h > HEIGHT)
+                ball.yVelocity = -1;
+
+            ball.rect.x += ball.xVelocity * ballSpeed;
+            ball.rect.y += ball.yVelocity * ballSpeed;
+
+            paddle_render(ren, &paddle);
+            ball_render(ren, &ball);
             break;
         case GAMEOVER:
             break;
